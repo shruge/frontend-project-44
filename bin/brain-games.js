388758f86703b8314f3e86f14f826ba392c878fb
ubuntu <mail4testsreq@gmail.com>
2024-08-12 11:47:20 +0300

    Add src/ , cli.js & Install readline-sync library

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
+import { getUserName } from "../src/cli.js";
+
+let userName = '';
 
 console.log("Welcome to the Brain Games!");
+
+userName = getUserName();
+
+console.log(`Hello, ${userName}!`);
