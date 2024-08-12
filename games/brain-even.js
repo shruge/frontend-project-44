5d92d7494c4bfe8ea7e569fcbfd7c15f7471c66c
ubuntu <mail4testsreq@gmail.com>
2024-08-12 20:03:39 +0300

    Add brain-calc.js, index.js

--- games/brain-even.js
+++ games/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { greetUser } from "../src/cli.js";
-import readlineSync from "readline-sync";
+import { checkAnswer } from "../src/index.js";
 
 let count = 0;
 const userName = greetUser();
@@ -9,24 +9,14 @@ console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
 for (let i = 0; i < 3; i++) {
    const rnd = Math.floor(Math.random() * 100);
-   
-   console.log(`Question: ${rnd}`);
-   
-   const answer = readlineSync.question("Your answer: ");
+    
    let correct = '';
 
    if (rnd % 2) correct = "no";
    else correct = "yes";
 
-   if (answer === correct) {
-     count+=1;
-
-     console.log("Correct!");
-   }
-   else {
-     console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correct}"`);
-     break;
-   }
+   if (checkAnswer(rnd, correct, userName)) count+=1;
+   else break;
 }
 
 if (count === 3) console.log(`Congratulations, ${userName}!`);
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import readlineSync from "readline-sync";
+
+export const checkAnswer = (question, correct, userName) => {
+	console.log(`Question: ${question}`);
+	
+	const userAnswer = readlineSync.question("Your answer: ");
+
+	if (userAnswer !== correct) {
+		console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correct}'.`);
+		console.log(`Let's try again, ${userName}!`);
+		
+		return false;
+	}
+
+	console.log("Correct!");
+	return true;
+}
